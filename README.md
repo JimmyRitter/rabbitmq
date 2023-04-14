404a8b267e7aa1a69a2e0d4cbbb4fe766664659a
Jimmy Ritter <jimmy.ritter@redacreltd.com>
2023-04-14 19:57:23 +0200

    publishing and consuming messages from queue; adding debugger config for VS Code;

---
+++ README.md
@@ -0,0 +1,12 @@
+# RabbitMQ tutorial
+
+## Requirements
+- Erlang
+- RabbitMQ service
+
+## Afer installation
+- RabbitMQ runs as a service on Windows. It must be manually started from services app
+
+## Description
+- There are 2 projects: Received (consumer) and Sender (publisher)
+- To run each of the projects, run `dotnet run` on each folder respectively 
--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "queue-name-1",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+// used to consume messages from a channel
+var consumer = new EventingBasicConsumer(channel);
+
+// attached the event handler, for when it received a new published message
+consumer.Received += (model, ea) =>
+{
+    // This extracts the message body from the event arguments and converts it to a byte array
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "queue-name-1",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
